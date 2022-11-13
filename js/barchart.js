00bb74a676c407033c6a863bc48c4760e437e850
Chris Stevenson <chris.stevenson@live.com>
2022-11-12 18:15:50 -0700

    Create rough barchart animation using chained transitions

--- js/barchart.js
+++ js/barchart.js
@@ -8,13 +8,78 @@ function displayYear(data) {
   let year = parseInt(d3.select("#year_selection").property("value"));
 
   function animateBarchart(data) {
-    const yearRange = d3.extent(
-      data.map((d) => {
-        return +d.Year;
-      })
+    const years = Array.from(new Set(data.map((d) => +d.Year))).sort(
+      (a, b) => a - b
     );
-    displayYear(data, yearRange[0]);
-    displayYear(data, yearRange[1]);
+    const color = d3
+      .scaleOrdinal(d3.schemeCategory10)
+      .domain(Array.from(new Set(data.map((d) => d.Platform))));
+    startDisplayChain(data, years, color);
+    return data;
+  }
+
+  function startDisplayChain(data, years, color, i = 0) {
+    const year = years[i];
+    const height = 400,
+      width = 600;
+    const margin = { top: 20, bottom: 20, left: 30, right: 20 };
+    const innerHeight = height - margin.top - margin.bottom,
+      innerWidth = width - margin.left - margin.right;
+    const filtered = data.filter((d) => {
+      return +d.Year === year;
+    });
+    const grouped = groupByPlatform(filtered);
+    const x = d3
+      .scaleBand()
+      .domain(grouped.map((g) => g.platform))
+      .range([0, innerWidth])
+      .padding(0.2);
+    const y = d3
+      .scaleLinear()
+      .range([innerHeight, 0])
+      .domain([0, Math.ceil(d3.max(grouped.map((g) => g.sales)))]);
+    const svg = d3.select("#barchart-svg");
+    svg
+      .selectAll("g.x-axis")
+      .data([year])
+      .join("g")
+      .classed("x-axis", true)
+      .attr(
+        "transform",
+        `translate(${margin.left}, ${innerHeight + margin.top})`
+      )
+      .transition("x-axis")
+      .duration(1000)
+      .call(d3.axisBottom(x));
+
+    svg
+      .selectAll("g.y-axis")
+      .data([year])
+      .join("g")
+      .classed("y-axis", true)
+      .attr("transform", `translate(${margin.left}, ${margin.top})`)
+      .transition("y-axis")
+      .duration(1000)
+      .call(d3.axisLeft(y));
+    svg
+      .selectAll("g.rects")
+      .data([year])
+      .join("g")
+      .classed("rects", true)
+      .attr("transform", `translate(${margin.left}, ${margin.top})`)
+      .selectAll("rect")
+      .data(grouped)
+      .join("rect")
+      .attr("width", x.bandwidth())
+      .attr("fill", (d) => color(d.platform))
+      .attr("y", (d) => y(d.sales))
+      .attr("height", (d) => y(0) - y(d.sales))
+      .transition("rects")
+      .attr("x", (d) => x(d.platform))
+      .duration(2000)
+      .on("end", () => {
+        if (i < years.length - 2) startDisplayChain(data, years, color, i + 1);
+      });
     return data;
   }
 
@@ -48,21 +113,25 @@ function displayYear(data) {
     .text((d) => d);
   svg
     .selectAll("g.x-axis")
-    .data([data])
+    .data([year])
     .join("g")
     .classed("x-axis", true)
     .attr("transform", `translate(${margin.left}, ${innerHeight + margin.top})`)
+    .transition("x-axis")
+    .duration(1000)
     .call(d3.axisBottom(x));
   svg
     .selectAll("g.y-axis")
-    .data([data])
+    .data([year])
     .join("g")
     .classed("y-axis", true)
     .attr("transform", `translate(${margin.left}, ${margin.top})`)
+    .transition("y-axis")
+    .duration(1000)
     .call(d3.axisLeft(y));
   svg
     .selectAll("g.rects")
-    .data([data])
+    .data([year])
     .join("g")
     .classed("rects", true)
     .attr("transform", `translate(${margin.left}, ${margin.top})`)
@@ -76,7 +145,7 @@ function displayYear(data) {
     .duration(1000)
     .attr("y", (d) => y(d.sales))
     .attr("height", (d) => y(0) - y(d.sales));
-  console.log(grouped);
+  return data;
 }
 
 function updateBarChart() {}
